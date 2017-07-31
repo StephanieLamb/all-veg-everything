1086e08bd68208e97545ca7aef69beb67c522aca
Stephanie Lamb <stephslamb@gmail.com>
2017-07-30 21:30:58 -0500

    started chatbot

--- js/recipefinder.js
+++ js/recipefinder.js
@@ -1,4 +1,41 @@
 $(function(){ 
+    var questionNum = 0;													// keep count of question, used for IF condition.
+    var question = '<h3>What would you like to make?</h3>';				  // first question
+
+    var output = document.getElementById('output');				// store id="output" in output variable
+    output.innerHTML = question;													// ouput first question
+
+    function bot() {
+        var input = document.getElementById("input").value;
+        console.log(input);
+
+        if (questionNum == 0) {
+            output.innerHTML = "<h3>Great...let's make " + input + "!</h3>";// output response
+            document.getElementById("input").value = "";   		// clear text box
+            question = '<h3>What is a fruit or vegetable you would like to use?</h3><button id="searchSubmit">Find Me Something Yummy</button>';			    	// load next question
+                setTimeout(timedQuestion, 2000);									// output next question after 2sec delay
+        }
+
+//     else if (questionNum == 1) {
+//     output.innerHTML = '<h1>That means you were born in ' + (2016 - input) + '</h1>';
+//     document.getElementById("input").value = "";
+//     question = '<h1>where are you from?</h1>';
+//     setTimeout(timedQuestion, 2000);
+//     }
+    }
+
+        function timedQuestion() {
+            output.innerHTML = question;
+        }
+
+// //push enter key (using jquery), to run bot function.
+        $(document).keypress(function(e) {
+            if (e.which == 13) {
+                bot();																						// run bot function when enter key pressed
+                questionNum++;																		// increase questionNum count by 1
+         }
+    });
+
 
 
 
@@ -12,10 +49,14 @@ $(function(){
         var results= [];
         
         recipes.forEach(function(recipe) {
-            var searchTerm = $("#searchTerm").val();
+            // var searchTerm = $("#searchTerm").val();
+            // if (recipe.keyword === searchTerm){
+            //     results.push(recipe);
+                
+            // }
+            var searchTerm = $("#input").val();
             if (recipe.keyword === searchTerm){
                 results.push(recipe);
-                
             }
 
         });
