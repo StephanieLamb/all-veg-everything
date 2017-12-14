c19c186bfffce00b667b49fadaa0f5ec5ec633eb
Stephanie Lamb <stephslamb@gmail.com>
2017-12-13 20:38:29 -0600

    fixed recipe card layout

--- js/recipefinder.js
+++ js/recipefinder.js
@@ -42,9 +42,24 @@ $(function(){
 
         function findRecipe() {
             var recipes = [
-                {name: 'Almond Butter Cookies', keyword: 'cookies', link: '<a href="https://minimalistbaker.com/almond-butter-chocolate-chip-cookies/" target="_blank" class="btn btn-warning btn-xs active" role="button">Almond Butter Cookies</a>', picture: '<img src="https://mbkr-minimalistbaker.netdna-ssl.com/wp-content/uploads/2017/05/AMAZING-Almond-Butter-Chocolate-Chip-Cookies-vegan-glutenfree-cookies-lowsugar-grainfree-768x1152.jpg" style="width:250px;height:250px;">'},
-                {name: 'Baked Sweet Potatoes', keyword: 'sweet potato', link: '<a href="https://minimalistbaker.com/baked-sweet-potatoes-2-ways/" target="_blank" class="btn btn-warning btn-xs active" role="button">Baked Sweet Potatoes</a>', picture: '<img src="https://mbkr-minimalistbaker.netdna-ssl.com/wp-content/uploads/2017/06/HEALTHY-Baked-Sweet-Potatoes-2-Ways-Savory-and-sweet-in-just-30-minutes-vegan-glutenfree-sweetpotato-healthy-minimalistbaker-dinner-768x1152.jpg" style="width:200px;height:250px;">'},
-                {name: 'Sweet Potato Tator Tots', keyword: 'sweet potato', link: '<a href="https://minimalistbaker.com/easy-baked-sweet-potato-tater-tots/" target="_blank" class="btn btn-warning btn-xs active" role="button">Sweet Potato Tator Tots</a>', picture: '<img src="https://mbkr-minimalistbaker.netdna-ssl.com/wp-content/uploads/2014/06/Easy-Baked-Sweet-Potato-Tater-Tots-7-ingredients-vegan-and-SUPER-delicious.jpg" style="width:200px;height:250px;">'},
+                {
+                    name: 'Almond Butter Cookies',
+                    keyword: 'cookies',
+                    link: '<a href="https://minimalistbaker.com/almond-butter-chocolate-chip-cookies/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Almond Butter Cookies</a>',
+                    picture: '<img src="https://mbkr-minimalistbaker.netdna-ssl.com/wp-content/uploads/2017/05/AMAZING-Almond-Butter-Chocolate-Chip-Cookies-vegan-glutenfree-cookies-lowsugar-grainfree-768x1152.jpg" display="block" style="width:250px;height:250px;">'
+                },
+                {
+                    name: 'Baked Sweet Potatoes',
+                    keyword: 'sweet potato',
+                    link: '<a href="https://minimalistbaker.com/baked-sweet-potatoes-2-ways/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Baked Sweet Potatoes</a>',
+                    picture: '<img src="https://mbkr-minimalistbaker.netdna-ssl.com/wp-content/uploads/2017/06/HEALTHY-Baked-Sweet-Potatoes-2-Ways-Savory-and-sweet-in-just-30-minutes-vegan-glutenfree-sweetpotato-healthy-minimalistbaker-dinner-768x1152.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Sweet Potato Tator Tots',
+                    keyword: 'sweet potato',
+                    link: '<a href="https://minimalistbaker.com/easy-baked-sweet-potato-tater-tots/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Sweet Potato Tator Tots</a>',
+                    picture: '<img src="https://mbkr-minimalistbaker.netdna-ssl.com/wp-content/uploads/2014/06/Easy-Baked-Sweet-Potato-Tater-Tots-7-ingredients-vegan-and-SUPER-delicious.jpg" display="block" style="width:200px;height:250px;">'
+                },
             ];
             var results= [];
         
@@ -65,11 +80,11 @@ $(function(){
             // if no results display "no results"
             if (recipes.length > 0){
                 var sectionBrowse = $("#sectionBrowse");
-                var recipeList = $("<ul>");
+                var recipeList = $("<ul class='recipeList'>");
                 var recipeCard = $("#recipeCard");
                 // var listItem = $("#recipeCard");
                 recipes.forEach(function(recipes) {
-                    var recipeCard = $(recipes.picture + recipes.link);
+                    var recipeCard = $("<li class='recipeListItem'><div>" + recipes.picture + recipes.link + "</div></li>");
                     
                     recipeList.append(recipeCard);
 
