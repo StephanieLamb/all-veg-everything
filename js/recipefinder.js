732fe90e960643437e0e1d342db558834b86fe51
Stephanie Lamb <stephslamb@gmail.com>
2018-01-08 19:47:34 -0600

    adding more recipes

--- js/recipefinder.js
+++ js/recipefinder.js
@@ -70,7 +70,7 @@ $(function(){
                     link: '<a href="https://minimalistbaker.com/the-worlds-easiest-cinnamon-rolls/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Vegan Cinnamon Rolls</a>',
                     picture: '<img src="https://minimalistbaker.com/wp-content/uploads/2013/09/EASY-7-Ingredient-Cinnamon-Rolls-minimalistbaker.com_.jpg" display="block" style="width:200px;height:250px;">'
                 },
-                 {
+                {
                     name: 'Peanut Butter Overnight Oats',
                     keyword: 'breakfast',
                     time: 'yes',
@@ -116,17 +116,17 @@ $(function(){
                     name: 'Banana Mocha Chip Muffins',
                     keyword: 'breakfast',
                     time: 'no',
-                    link: '<a href="https://www.feastingonfruit.com/banana-mocha-chip-muffins/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Banana Moch Chip Muffins</a>',
+                    link: '<a href="https://www.feastingonfruit.com/banana-mocha-chip-muffins/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Banana Mocha Chip Muffins</a>',
                     picture: '<img src="https://i2.wp.com/www.feastingonfruit.com/wp-content/uploads/2017/10/Banana-Mocha-Chip-Muffins3.jpg?w=1000&ssl=1" display="block" style="width:200px;height:250px;">'
                 },
-                   {
+                {
                     name: 'Chia Spiced Pancakes',
                     keyword: 'breakfast',
                     time: 'yes',
                     link: '<a href="http://myvibrantkitchen.com/vegan-chai-spice-pancakes-whipped-maple-butter/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Chia Spiced Pancakes</a>',
                     picture: '<img src="https://i2.wp.com/myvibrantkitchen.com/wp-content/uploads/2017/11/vegan-chai-spiced-pancakes-whipped-maple-butter-4.jpg?resize=900%2C1350" display="block" style="width:200px;height:250px;">'
                 },
-                 {
+                {
                     name: 'Blueberry Pancakes',
                     keyword: 'breakfast',
                     time: 'yes',
@@ -140,6 +140,168 @@ $(function(){
                     link: '<a href="http://www.thelittleblogofvegan.com/2017/07/gluten-free-vegan-chocolate-nutella.html" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Nutella Pancakes</a>',
                     picture: '<img src="https://4.bp.blogspot.com/-5JoPEhRlNiE/WV4mI37ZsKI/AAAAAAAASmU/5rmAJ280ETIle1eswd5-0RdjRpK95_i4gCLcBGAs/s1600/nutella_pancakes_vegan.jpg" display="block" style="width:200px;height:250px;">'
                 },
+                 {
+                    name: 'Sweet Potato and Crispy Quinoa Salad',
+                    keyword: 'main',
+                    time: 'no',
+                    link: '<a href="https://wendypolisi.com/sweet-potato-pomegranate-crispy-quinoa-salad/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Sweet Potato and Quinoa Salad</a>',
+                    picture: '<img src="https://wendypolisi.com/wp-content/uploads/2016/01/sweet-potato-pomegranate-crispy-quinoa-salad.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Vegan Couscous Salad',
+                    keyword: 'main',
+                    time: 'yes',
+                    link: '<a href="https://www.lazycatkitchen.com/vegan-couscous-salad-roasted-parsnips/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Vegan Couscous Salad</a>',
+                    picture: '<img src="https://cdn77-s3.lazycatkitchen.com/wp-content/uploads/2018/01/vegan-couscous-salad-lunch-800x1200.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Detox Salad with Lentils',
+                    keyword: 'main',
+                    time: 'yes',
+                    link: '<a href="http://www.occasionallyeggs.com/2018/01/cold-weather-detox-salad-with-crispy.html" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Detox Salad with Lentils</a>',
+                    picture: '<img src="https://4.bp.blogspot.com/-1yZXVKVe1MI/Wku7oQniqiI/AAAAAAAADAo/PVPkgE1lM8w4kuXODwUdWBdObRQEoJLBACLcBGAs/s1600/vegan-detox-salad.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Kale Salad with Avocado Dressing',
+                    keyword: 'main',
+                    time: 'yes',
+                    link: '<a href="http://www.3scoopsofsugar.com/kale-carrot-cabbage-salad-with-avocado-dressing/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Kale Salad with Avocado Dressing</a>',
+                    picture: '<img src="https://i0.wp.com/www.3scoopsofsugar.com/wp-content/uploads/2017/12/kalecarrotcabbagesalad15ps.jpg?w=667" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Confetti Beet Salad',
+                    keyword: 'main',
+                    time: 'no',
+                    link: '<a href="https://www.spicesinmydna.com/2017/12/27/confetti-beet-salad/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Confetti Beet Salad</a>',
+                    picture: '<img src="https://www.spicesinmydna.com/wp-content/uploads/2017/12/5F4A7243-3.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Pasta with Porcini Bolognese',
+                    keyword: 'main',
+                    time: 'no',
+                    link: '<a href="http://www.euphoricvegan.com/2018/01/pappardelle-with-porcini-bolognese.html" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Pasta with Porcini Bolognese</a>',
+                    picture: '<img src="https://1.bp.blogspot.com/-DHnmSlUMccY/WlEtwI8bxEI/AAAAAAAAS0Y/KlYvZC7E29Qh-LgQinx2MIM_VXBK47XAQCLcBGAs/s1600/IMGP0438_edited1.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Pasta Carbonara',
+                    keyword: 'main',
+                    time: 'yes',
+                    link: '<a href="https://simpleveganblog.com/vegan-pasta-carbonara/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Pasta Carbonara</a>',
+                    picture: '<img src="https://simpleveganblog-lne9w9dshg8v.stackpathdns.com/wp-content/uploads/2018/07/Vegan-pasta-carbonara-2.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Soy Ginger Soba Noodles',
+                    keyword: 'main',
+                    time: 'yes',
+                    link: '<a href="http://thewoodenskillet.com/soy-ginger-soba-noodles-avocado-edamame/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Soy Ginger Soba Noodles</a>',
+                    picture: '<img src="https://i2.wp.com/thewoodenskillet.com/wp-content/uploads/2016/11/1-IMG_9766.jpg?w=1067" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Vegan Mac and Cheese',
+                    keyword: 'main',
+                    time: 'yes',
+                    link: '<a href="https://www.connoisseurusveg.com/vegan-mac-and-cheese/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Vegan Mac and Cheese</a>',
+                    picture: '<img src="https://www.connoisseurusveg.com/wp-content/uploads/2017/12/vegan-mac-and-cheese-2-of-3.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Broccoli Pesto with Pasta',
+                    keyword: 'main',
+                    time: 'yes',
+                    link: '<a href="https://veganheaven.org/recipe/broccoli-pesto-with-pasta-and-cherry-tomatoes/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Broccoli Pesto with Pasta</a>',
+                    picture: '<img src="https://veganheaven.org/wp-content/uploads/2017/12/Broccoli-Pesto-with-Pasta-and-Cherry-Tomatoes-3.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Spinach Pesto Lasagna',
+                    keyword: 'main',
+                    time: 'no',
+                    link: '<a href="https://vegetariangastronomy.com/spinach-pesto-lasagna-creamy-mushroom-sauce-vegan/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Spinach Pesto Lasagna</a>',
+                    picture: '<img src="https://vegetariangastronomy.com/wp-content/uploads/2017/12/creamy-spinach-mushroom-lasagna-vegan-7.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Black Bean Veggie Burger',
+                    keyword: 'main',
+                    time: 'no',
+                    link: '<a href="https://www.noracooks.com/the-best-black-bean-veggie-burger/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Black Bean Veggie Burger</a>',
+                    picture: '<img src="https://i1.wp.com/www.noracooks.com/wp-content/uploads/2017/12/IMG_4675.jpg?w=667&ssl=1" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Buffalo Tofu Sandwich',
+                    keyword: 'main',
+                    time: 'yes',
+                    link: '<a href="http://myvibrantkitchen.com/vegan-buffalo-tofu-chicken-sandwich/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Buffalo Tofu Sandwich</a>',
+                    picture: '<img src="https://i1.wp.com/myvibrantkitchen.com/wp-content/uploads/2018/01/vegan-buffalo-sauce-tofu-chicken-sandwich-1.jpg?resize=900%2C1350" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Chickpea Hummus Wrap',
+                    keyword: 'main',
+                    time: 'no',
+                    link: '<a href="http://www.pepperdelight.com/roasted-chickpeas-hummus-wrap/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Chickpea Hummus Wrap</a>',
+                    picture: '<img src="https://i1.wp.com/www.pepperdelight.com/wp-content/uploads/2017/12/pepper-delight-hummus-wrap-1.jpg?w=1516" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Jackfruit Carnitas Tacos',
+                    keyword: 'main',
+                    time: 'yes',
+                    link: '<a href="http://www.kalememaybe.com/kalememaybe/2017/10/16/jackfruit-carnitas-tacos" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Jackfruit Carnitas Tacos</a>',
+                    picture: '<img src="https://static1.squarespace.com/static/554d5698e4b0e4053a33b9e5/t/59e58a7dccc5c53e1f0986cb/1508215552769/IMG_8785.JPG?format=750w" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Roasted Cauliflower Tacos',
+                    keyword: 'main',
+                    time: 'no',
+                    link: '<a href="http://www.laureninbalance.com/quinoa-roasted-cauliflower-tacos-with-cashew-roasted-red-pepper-sauce/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Roasted Cauliflower Tacos</a>',
+                    picture: '<img src="http://www.laureninbalance.com/wp-content/uploads/2017/10/roasted-cauliflower-tacos-1.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Avocado Fries',
+                    keyword: 'snack',
+                    time: 'yes',
+                    link: '<a href="https://mindfulavocado.com/avocado-fries-four-ingredients/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Avocado Fries</a>',
+                    picture: '<img src="https://mindfulavocado.com/wp-content/uploads/2018/01/avocado-fries_006.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Crispy Rice Squares',
+                    keyword: 'snack',
+                    time: 'no',
+                    link: '<a href="https://www.whoneedssalad.com/crispy-rice-squares/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Crispy Rice Squares</a>',
+                    picture: '<img src="https://i0.wp.com/www.whoneedssalad.com/wp-content/uploads/2018/01/crispy-rice-squares.jpg?w=1616&ssl=1" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Caramel Peanut Butter Truffles',
+                    keyword: 'snack',
+                    time: 'no',
+                    link: '<a href="http://oatsodelicious.com/2017/12/29/salted-caramel-peanut-butter-truffles/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Caramel Peanut Butter Truffles</a>',
+                    picture: '<img src="http://oatsodelicious.com/wp-content/uploads/2017/12/IMG_20171229_151933-750x867.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Corn Fritters',
+                    keyword: 'snack',
+                    time: 'no',
+                    link: '<a href="https://www.cookclickndevour.com/corn-fritters-recipe-no-deep-fry-sweet-corn-fritters-recipe" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Corn Fritters</a>',
+                    picture: '<img src="https://www.cookclickndevour.com/wp-content/uploads/2016/07/corn-fritters-recipe-c1.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Thai Peanut Toasty',
+                    keyword: 'snack',
+                    time: 'yes',
+                    link: '<a href="http://www.chefpriyanka.com/thai-peanut-toasty/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Thai Peanut Toasty</a>',
+                    picture: '<img src="http://www.chefpriyanka.com/wp-content/uploads/2017/10/thai-toasty-1.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'Vegan Bavarian Pretzels',
+                    keyword: 'snack',
+                    time: 'no',
+                    link: '<a href="https://wellvegan.com/snack/vegan-bavarian-pretzels-oktoberfest" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">Vegan Bavarian Pretzels</a>',
+                    picture: '<img src="https://bg6fhgmkni-flywheel.netdna-ssl.com/wp-content/uploads/2017/09/bavarian-pretzels-3@15x-683x1024.jpg" display="block" style="width:200px;height:250px;">'
+                },
+                {
+                    name: 'The Best Hummus',
+                    keyword: 'snack',
+                    time: 'yes',
+                    link: '<a href="https://mindfulavocado.com/best-hummus-recipe/" id="buttonLink" target="_blank" class="btn btn-warning btn-xs active" role="button">The Best Hummus</a>',
+                    picture: '<img src="https://mindfulavocado.com/wp-content/uploads/2017/09/hummus_004.jpg" display="block" style="width:200px;height:250px;">'
+                },
+
             ];
             var results= [];
         
